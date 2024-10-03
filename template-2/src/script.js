e3eb9bc6fa6ca60edbe84a758fa6e1d59748012b
Igor Sousa <igor2001silva3@gmail.com>
2024-10-03 17:13:02 -0300

    Responsividade Site e JS de troca de página

---
+++ template-2/src/script.js
@@ -0,0 +1,30 @@
+
+class PageLoader {
+    constructor() {
+        this.mainContent = document.getElementById('main-content');;
+    }
+
+    async loadContent(page) {
+        try {
+            const response = await fetch(`${page}.html`);
+            if (!response.ok) throw new Error(`Erro ao carregar ${page}`);
+            this.mainContent.innerHTML = await response.text();
+            this.initializeProductEvents(); // Inicializar eventos após o carregamento da página
+        } catch (error) {
+            this.mainContent.innerHTML = `<p>Erro ao carregar a página: ${error.message}</p>`;
+        }
+    }
+
+    initializeProductEvents() {
+        const products = document.querySelectorAll('.wrap-product');
+        products.forEach((item) => {
+            item.addEventListener('click', (event) => {
+                console.log(event);
+                this.loadContent('pages/product');
+            });
+        });
+    }
+}
+
+const pageLoader = new PageLoader();
+pageLoader.loadContent('pages/home');
